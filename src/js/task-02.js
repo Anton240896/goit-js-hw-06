feda7a0c2eb7fa36a46a6939da254a681c01fedb
Anton240896 <antalipov24081111111@gmail.com>
2023-07-15 22:03:08 +0300

    add

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,4 +8,13 @@ const ingredients = [
 ];
 
 
-console.log(ingredients);
+const ul = document.querySelector('#ingredients'); 
+
+                    ingredients.forEach(ingredient => { // перебираємо масив
+
+const li = document.createElement('li'); // створюємо окремий елемент li
+li.textContent = ingredient; // текстовий вміст , тобто список на html сторінці 
+  li.classList.add('item'); // додаємо до елемента li , Css клас item
+  ul.append(li); // Вставляємо усі li  за одну операцію у список ul#ingredients.
+ 
+});
