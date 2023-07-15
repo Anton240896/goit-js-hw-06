41c5bed0fc88b72fa6cbd1b6e70c5fa85ca812af
Anton240896 <antalipov24081111111@gmail.com>
2023-07-15 08:16:36 +0300

    add

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,13 @@
-const numberCategories = document.querySelectorAll('.item');
+const listCategories = document.querySelector('#categories');
+const categories = listCategories.querySelectorAll('.item');
+
+console.log('Number of categories:',  categories.length);
+
+
+categories.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const elementsAmount = category.querySelectorAll('li').length;
+
+  console.log('Category:', categoryName);
+  console.log('Elements:', elementsAmount);
+});
