c8eba27ffcbe5ef534f0f8fff42674c509679957
Anton240896 <antalipov24081111111@gmail.com>
2023-07-16 19:09:39 +0300

    add

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -13,7 +13,7 @@ const ul = document.querySelector('#ingredients');
                     ingredients.forEach(ingredient => { // перебираємо масив
 
 const li = document.createElement('li'); // створюємо окремий елемент li
-li.textContent = ingredient; // текстовий вміст , тобто список на html сторінці 
+li.textContent = ingredient; // текстовий вміст 
   li.classList.add('item'); // додаємо до елемента li , Css клас item
   ul.append(li); // Вставляємо усі li  за одну операцію у список ul#ingredients.
  
