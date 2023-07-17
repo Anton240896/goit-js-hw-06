05ff1ca6dd930131ed8087f80db30316addb3b3f
Anton240896 <antalipov24081111111@gmail.com>
2023-07-17 20:21:07 +0300

    add

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -3,7 +3,7 @@ let counterValue = 0;
 
 const decrement = document.querySelector("button[data-action ='decrement']")
 const increment = document.querySelector("button[data-action ='increment']")
-const counter = document.getElementById('value');
+const counter = document.getElementById('#value');
 
 decrement.addEventListener("click", () => {
     counterValue -= 1;
