ad1824e818ef0a7decfbbf47e17db30ee187c8b9
Anton240896 <antalipov24081111111@gmail.com>
2023-07-18 12:15:01 +0300

    add

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,16 +1,16 @@
-let counterValue = 0;
+let counterValue = 0; // створюєм let, тому що буде змінюватись
 
 const decrement = document.querySelector('button[data-action="decrement"]');
 const increment = document.querySelector('button[data-action="increment"]');
 const value = document.querySelector('#value');
 
-decrement.addEventListener('click', () => {
-    counterValue -= 1;
+decrement.addEventListener('click', () => { // додаємо подію 
+    counterValue -= 1; // віднімаємо від let 
     value.textContent = counterValue;
 });
 
 
 increment.addEventListener('click', () => {
-    counterValue += 1;
+    counterValue += 1; // додаємо до let
     value.textContent = counterValue;
 })
