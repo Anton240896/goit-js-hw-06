88ef9576778f16470a5f3ce078175bbe628390f9
Anton240896 <antalipov24081111111@gmail.com>
2023-07-18 12:47:47 +0300

    add

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,1 +1,16 @@
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
+
+input.addEventListener('input', inputChange);
+
+function inputChange(meaning) {
+    if (meaning.currentTarget.value) {
+        output.textContent = meaning.currentTarget.value;
+    }
+
+    else {
+  output.textContent = "Anonymous";
+
+    }
+}
