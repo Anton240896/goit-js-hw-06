edbebde7bc06f9df1c8e581cd424c8537155a210
Anton240896 <antalipov24081111111@gmail.com>
2023-07-18 13:43:43 +0300

    add

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,16 +1,19 @@
 
 
 
-const input = document.querySelector('#name-input');   
-const output = document.querySelector('#name-output'); 
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
 
-input.addEventListener('input', changeName);
-   
-    function changeName(meaning) {
-        if (meaning.currentTarget.value) {
-          output.textContent = meaning.currentTarget.value;
 
-        } else
-         output.textContent = 'Anonymous';
-      }
-    
+input.addEventListener('input', changeValue);
+
+function changeValue(meaning) {
+  
+if (input.value === '') {
+    output.textContent = 'Anonymous'; 
+  }
+
+   else {
+    output.textContent = input.value;
+  }
+};
